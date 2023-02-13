13290f3d62f127e2cec2589d76ee07a00a48366e
opilane <opilane@HP-CZC0359NNC>
2023-02-13 14:44:52 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var domains = new Dictionary<string, string>
+            {
+                {"est", "Estonia"},
+                {"lat", "Latvia" },
+                {"fin", "Finland" },
+                {"swe", "Sweden" },
+            };
+            int i = 1;
+            foreach (var pair in domains)
+            {
+                Console.WriteLine($"{pair.Key} - {pair.Value} - {i}");
+                i++;
+            }
+        }
+    }
+}
